c63f04c8929ff7babdddbbd4330cfc63d424f219
AdvancingHermit <christiannkya@outlook.com>
2024-11-03 17:23:00 +0100

    Register File works I think

--- src/main/scala/RegisterFile.scala
+++ src/main/scala/RegisterFile.scala
@@ -4,8 +4,21 @@ import chisel3.util._
 class RegisterFile extends Module {
   val io = IO(new Bundle {
     //Define the module interface here (inputs/outputs)
+    val aSel = Input(UInt(5.W))
+    val bSel = Input(UInt(5.W))
+    val writeData = Input(UInt(32.W))
+    val writeSel = Input(UInt(4.W))
+    val writeEnable = Input(UInt(1.W))
+    val a = Output(UInt(32.W))
+    val b = Output(UInt(32.W))
   })
-
   //Implement this module here
+  private val registerFile = Reg(Vec(32, UInt(32.W)))
 
+  when(io.writeEnable === 1.U(1.W)){
+    registerFile(io.writeSel) := io.writeData
+  }
+
+  io.a := registerFile(io.aSel)
+  io.b := registerFile(io.bSel)
 }
